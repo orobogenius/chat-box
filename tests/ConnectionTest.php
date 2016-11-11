b6ed58cbf0a1542788ff3821090ac4d1bf34dd5b
Orobo Lucky Ozoka <orobogenius@gmail.com>
2016-11-11 23:57:41 +0100

    Add files via upload

---
+++ tests/ConnectionTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace Chat;
+
+
+require 'src/chat/connection.php';
+
+class ConnectionTest Extends \PHPUnit_Framework_TestCase
+{
+  public function testSetConnectionProperties()
+  {
+    $dbh = new Connection("localhost", "chatdb", "root", "this.mysql");
+    $this->assertAttributeEquals("localhost", "server", $dbh);
+    $this->assertAttributeEquals("chatdb", "database", $dbh);
+    $this->assertAttributeEquals("root", "username", $dbh);
+    $this->assertAttributeEquals("this.mysql", "password", $dbh);
+  }
+
+  public function testConnect()
+  {
+    $dbh = new Connection("localhost", "chatdb", "root", "this.mysql");
+    $hn = $dbh->connect();
+    $this->assertInstanceOf(\PDO::class, $hn);
+  }
+
+  public function testCloseConnection()
+  {
+    $dbh = new Connection("localhost", "chatdb", "root", "this.mysql");
+    $hn = $dbh->connect();
+    $hn = $dbh->close();
+    $this->assertNull($hn);
+  }
+
+}
